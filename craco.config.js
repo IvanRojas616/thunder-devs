79c34373db558254d3059c91ca7c6ea2ccf8a911
Ivan Rojas <ivanrojas616lost@gmail.com>
2021-10-29 16:51:07 -0500

    config in craco for deploy in s3 amazon

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,23 @@
 // craco.config.js
+//it's needed use craco-alias and this config for S3 AWS
+const CracoAlias = require('craco-alias');
+
 module.exports = {
-    style: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
+  style: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'options',
+        baseUrl: './',
+        aliases: {
+          '*': './src',
+        },
       },
     },
-  }
+  ],
+};
